3c1053212dd9983369e9807290801bf55543d3d7
dvrnc <darryvoron1404@gmail.com>
2024-11-27 21:21:09 +0300

    Main in dev update 2

--- Main.java
+++ Main.java
@@ -3,5 +3,6 @@ class Main {
     public static void main(String[] args) {
         System.out.print("Это добавлено в ветке dev");
         System.out.print("Это добавлено в ветке dev 1");
+        System.out.print("Это добавлено в ветке dev 2");
     }
 }
